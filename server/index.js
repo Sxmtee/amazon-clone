0862f6ad9bff0af8d769d357fd99f5132e3e9dc9
sxmtee <somtoo76@gmail.com>
2023-03-18 22:18:49 +0100

    auth

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
+//IMPORTS FROM PACKAGES
 const express = require("express");
+
+//IMPORTS FROM OTHER FILES
+const authRouter = require("./routes/auth");
+
+//INIT
 const app = express();
 const PORT = 3000;
 
+//middleware
+app.use(authRouter);
+
 //CREATING AN API
 //Get, Put, Post, Delete, Update -> CRUD
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`connected at port ${PORT}`);
+
+app.listen(PORT, () => {
+  console.log(`connected at port ${PORT}`);
 });
---
+++ server/routes/auth.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const authRouter = express.Router();
+
+authRouter.get();
+
+module.exports = authRouter;
