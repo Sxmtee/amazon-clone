7636014f53c7bd7b52042a90ed74f3bf1b4e60a8
sxmtee <somtoo76@gmail.com>
2023-03-20 17:41:05 +0100

    uri

--- lib/Constants/global_variable.dart
+++ lib/Constants/global_variable.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-String uri = 'http://<yourip>:3000';
+String uri = 'http://192.168.43.38:3000';
 
 class GlobalVariables {
   // COLORS
--- lib/Features/Auth/Services/auth_services.dart
+++ lib/Features/Auth/Services/auth_services.dart
@@ -1,8 +1,20 @@
-class AuthService{
+import 'package:amazon/Constants/global_variable.dart';
+import 'package:amazon/Models/user.dart';
+import 'package:http/http.dart' as http;
 
+class AuthService {
   //sign-up user
   void signUpUser({
     required String email,
     required String password,
-  })
+    required String name,
+  }) async {
+    try {
+      User user = User(
+          address: "", id: "", name: "", password: "", token: "", type: "");
+
+      var url = Uri.parse("$uri/api/signup");
+      http.post(url);
+    } catch (e) {}
+  }
 }
